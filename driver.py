f089a575b3e2524db2b3bb50affccc88846cf10c
jvictor1223 <91972496+jvictor1223@users.noreply.github.com>
2022-04-27 13:55:07 -0300

    Update driver.py

--- driver.py
+++ driver.py
@@ -1,13 +1,15 @@
 #JOAO CALIXTO
 
 from wsgiref.validate import validator
+from timeit import default_timer as timer
+
 import board
 
 #Tabuleiro a ser manipulado
 
 
 slots = int(input("Número de espaços no tabuleiro(ixj): "))
-
+start = timer()
 b = board.Board(slots)
 
 #retorna TRUE se o tabuleiro existir a possibilidade de N queens e deixa b.queenspaces completas com espaço apropriado FALSE caso o contrario
@@ -42,3 +44,5 @@ def placeMoves():
 if __name__ == "__main__":
     placeMoves()
     b.print()
+end = timer()
+print(end - start)
